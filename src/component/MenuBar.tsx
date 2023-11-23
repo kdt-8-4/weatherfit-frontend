1c8182872b3837e7921b95279b54428354fdade1
hyeiis <hyeis0807@gmail.com>
2023-11-23 09:08:16 +0900

    [Feat] #29 메뉴바 페이지 이동 Link 형식으로 수정

--- src/component/MenuBar.tsx
+++ src/component/MenuBar.tsx
@@ -4,39 +4,41 @@ import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import CheckroomOutlinedIcon from "@mui/icons-material/CheckroomOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import Link from "next/link";
 
 export default function Menubar() {
-  const router = useRouter(); // useRouter 훅을 사용하여 router 객체 가져오기
-  const goto_home = () => {
-    router.push("/"); // '/'로 페이지 이동
-  };
-  const goto_feed = () => {
-    router.push("/feed"); // '/feed'로 페이지 이동
-  };
-  const goto_upload = () => {
-    router.push("/upload"); // '/upload'로 페이지 이동
-  };
-  const goto_myPage = () => {
-    router.push("/mypage"); // '/mypage'로 페이지 이동
-  };
+  // const router = useRouter(); // useRouter 훅을 사용하여 router 객체 가져오기
+  // const goto_home = () => {
+  //   router.push("/"); // '/'로 페이지 이동
+  // };
+  // const goto_feed = () => {
+  //   router.push("/feed"); // '/feed'로 페이지 이동
+  // };
+  // const goto_upload = () => {
+  //   router.push("/upload"); // '/upload'로 페이지 이동
+  // };
+  // const goto_myPage = () => {
+  //   router.push("/mypage"); // '/mypage'로 페이지 이동
+  // };
+
   return (
     <div className="menubar">
-      <div className="menu_box" onClick={goto_home}>
+      <Link href="/" className="menu_box">
         <HomeOutlinedIcon className="menu_icon" />
         <span>홈</span>
-      </div>
-      <div className="menu_box" onClick={goto_feed}>
+      </Link>
+      <Link href="/feed" className="menu_box">
         <CheckroomOutlinedIcon className="menu_icon" />
         <span>구경</span>
-      </div>
-      <div className="menu_box" onClick={goto_upload}>
+      </Link>
+      <Link href="/upload" className="menu_box">
         <AddBoxOutlinedIcon className="menu_icon" />
         <span>업로드</span>
-      </div>
-      <div className="menu_box" onClick={goto_myPage}>
+      </Link>
+      <Link href="mypage" className="menu_box">
         <PersonOutlineOutlinedIcon className="menu_icon" />
         <span>마이페이지</span>
-      </div>
+      </Link>
     </div>
   );
 }
