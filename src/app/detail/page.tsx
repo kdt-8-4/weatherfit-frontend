420076aa15c740ec66ad9e074350f6819b55ab6f
hyeiis <hyeis0807@gmail.com>
2023-11-25 13:42:36 +0900

    [Feat] #56 게시물 상세 페이지 구현 및 전체파일 디자인 변경

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -2,16 +2,40 @@
 
 import WeatherBar from "@/component/WeatherBar";
 import Image from "next/image";
+import "../../style/detail.scss";
+import Menubar from "@/component/MenuBar";
+import Profile from "@/component/Profile";
+import ImageDetail from "@/component/ImageDetail";
 
 export default function Detail(): JSX.Element {
   return (
     <div className="container">
-      <div className="w-full h-12 flex items-center ">
-        <Image src="/images/back.svg" width={15} height={15} alt="back" />
-      </div>
-      <hr className="w-full h-px" />
-      <WeatherBar />
-      <hr className="w-full h-px" />
+      <header className="top w-full">
+        <div className="w-full h-12 flex items-center ">
+          <Image
+            src="/images/back.svg"
+            width={15}
+            height={15}
+            className="ml-3 cursor-pointer"
+            alt="back"
+            onClick={() => {
+              window.history.back();
+            }}
+          />
+        </div>
+        <hr className="w-full h-px" />
+        <WeatherBar />
+        <hr className="w-full h-px" />
+      </header>
+
+      <section className="main w-full h-full">
+        <Profile />
+        <ImageDetail />
+      </section>
+
+      <footer className="w-full">
+        <Menubar />
+      </footer>
     </div>
   );
 }
