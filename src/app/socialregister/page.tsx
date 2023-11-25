e032e02c03ed3bb461f3528f3b52bbf68e77e5ba
hyeiis <hyeis0807@gmail.com>
2023-11-25 10:42:43 +0900

    [Fix] #34 불필요한 코드 삭제

--- src/app/socialregister/page.tsx
+++ src/app/socialregister/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
@@ -8,7 +7,6 @@ export default function CompleteProfile() {
   const [name, setName] = useState<string>("");
   const [nickname, setNickname] = useState<string>("");
   const router = useRouter();
-  const { data: session } = useSession();
 
   // 닉네임 설정 완료 후 다음 단계로 이동
   const handleNicknameSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -27,11 +25,6 @@ export default function CompleteProfile() {
       console.error("닉네임 저장 오류:", error);
     }
   };
-
-  if (!session) {
-    router.push("/login");
-    return null;
-  }
 
   return (
     <div>
