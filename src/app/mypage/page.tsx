6720c9732e1ead567d98f3f2dc17ec619fc3c3a6
nebulaBdj <dongdung5070@gmail.com>
2023-11-30 10:51:08 +0900

    [Feat]로그인 토큰 존재할떄만 마이페이지 랜더링 (#9)

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import "../../style/mypage.scss";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
@@ -7,19 +7,48 @@ import Menubar from "@/component/MenuBar";
 import TabBar from "@/component/TabBar";
 import ProfileModal from "@/component/ProfileModal";
 
+import Cookies from "js-cookie";
+import Link from "next/link";
+
+
 // import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined'; // > 아이콘
 
 export default function Mypage() {
   // 회원 정보 수정 모달
   const [showProfileModify, setShowProfileModify] = useState<boolean>(false);
+  // 로그인 확인 후 페이지 로드
+  const [logincheck, setCheck] = useState<boolean>(false);
+  // 토큰 값
+  const [logintoken, setToken] = useState<string | undefined>("");
+
+  const cookie = () => {
+    const accessToken = Cookies.get("accessToken");
+    console.log("accessToken 값: ", accessToken);
+    setToken(accessToken);
+  }
+
+  useEffect(()=>{
+    //쿠키 가져오기
+    cookie();
+    if(logintoken === undefined) {
+      setCheck(false);
+    } else {
+      setCheck(true);
+    }
+
+  },[logintoken]);
 
   // 회원 정보 수정 모달 이벤트
   const handleSettingsClick = () => {
     setShowProfileModify(!showProfileModify);
   };
 
+  console.log('로그인 토큰 존재 확인', logincheck);
+  console.log('로그인 토큰 값', logintoken);
+
   return (
     <>
+      { logincheck ? 
       <div className="container">
         
         {/* header 넣을지 말지 */}
@@ -51,10 +80,18 @@ export default function Mypage() {
           <TabBar />
         </div>
         <Menubar />
-      </div>
+      </div>  : 
+      <>
+        <div>로그인을 해주세요.</div>
+        <Link href={'/login'}>로그인 페이지로 이동</Link>
+      </>
+      }
+      
       {showProfileModify && (
         <ProfileModal handleSettingsClick={handleSettingsClick} />
       )}
+    
+      
     </>
   );
 }
--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -23,17 +23,17 @@ export default function LoginForm() {
   // }, []);
 
   const onGoogleSocialLogin = async () => {
-    // try {
-    //   const response = await axios({
-    //     method: "GET",
-    //     url: "https://jerneithe.site/user/social/login/google",
-    //   });
-    //   console.log(response);
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    window.location.href =
-      "https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&include_granted_scopes=true&response_type=token&redirect_uri=https://www.jerneithe.site/user/login/oauth2/code/google&client_id=453423602833-7db2b1dbicre47rkcrpfgn20nd16l9rs.apps.googleusercontent.com";
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "https://accounts.google.com/o/oauth2/v2/auth?client_id=453423602833-7db2b1dbicre47rkcrpfgn20nd16l9rs.apps.googleusercontent.com&redirect_uri=https://localhost:3000&response_type=token&scope=email",
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    // window.location.href =
+    //   "https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&include_granted_scopes=true&response_type=token&redirect_uri=https://www.jerneithe.site/user/login/oauth2/code/google&client_id=453423602833-7db2b1dbicre47rkcrpfgn20nd16l9rs.apps.googleusercontent.com";
   };
   const handleLogin = async (e: FormEvent) => {
     e.preventDefault();
@@ -57,7 +57,7 @@ export default function LoginForm() {
       // 토큰을 쿠키에 저장
       document.cookie = `accessToken=${resData.token}; path=/`;
       setToken(resData.token);
-      // router.push('/');
+      router.push('/');
     } catch (error: any) {
       setEmail("");
       setPw("");
@@ -113,6 +113,8 @@ export default function LoginForm() {
       <div className="login_easy">
         <hr /> 간편 로그인 <hr />
         <button onClick={onGoogleSocialLogin}>구글 로그인</button>
+        <hr />
+        <button>카카오 로그인 </button>
       </div>
     </>
   );
