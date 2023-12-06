856c3751c48db4b4d9eb6eea461d60350b2ef6bb
hyeiis <hyeis0807@gmail.com>
2023-12-06 21:34:20 +0900

    [Fix] #118, #8, #9 기능 및 css 적

--- src/component/BestCoordi.tsx
+++ src/component/BestCoordi.tsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { WeatherState } from "@/recoilAtom/WeatherState";
 import "@/style/best_main.scss";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 interface Board {
   boardId: number;
@@ -20,6 +20,19 @@ export default function BestCoordi() {
   const [boards, setBoards] = useState<Board[]>([]);
   const weather = useRecoilValue(WeatherState);
   const router = useRouter();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === boards.length - 1 ? 0 : prevIndex + 1,
+    );
+  };
+
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? boards.length - 1 : prevIndex - 1,
+    );
+  };
 
   const sendDetail = async (board_id: number) => {
     console.log("게시글 아이디", board_id);
@@ -33,7 +46,8 @@ export default function BestCoordi() {
       try {
         const response = await axios({
           method: "GET",
-          url: `https://www.jerneithe.site/board/tops?temp_min=${weather.min}&temp_max=${weather.max}`,
+          // url: `https://www.jerneithe.site/board/tops?temp_min=${weather.min}&temp_max=${weather.max}`,
+          url: `https://www.jerneithe.site/board/tops?temp_min=0&temp_max=10`,
         });
 
         setBoards(response.data.content);
@@ -51,29 +65,30 @@ export default function BestCoordi() {
       <h2>
         오늘 날씨, <span className="highlight">좋아요가 가장 많은</span> 코디는?
       </h2>
-      <div className="boardTop5">
-        {boards.map((board, index) => (
-          <div
-            key={index}
-            onClick={() => sendDetail(board.boardId)}
-            style={{ cursor: "pointer", width: "100%", height: "100%" }}>
-            <div
-              className="image-wrapper"
-              style={{
-                position: "relative",
-                paddingBottom: "100%",
-                width: "100%",
-                height: "100%",
-              }}>
-              <Image
-                src={board.images.imageUrl}
-                alt={`Board ${board.boardId}`}
-                layout="fill"
-                objectFit="contain"
-              />
+      <div className="carousel">
+        <div
+          className="carousel-inner"
+          style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+          {boards.map((board, index) => (
+            <div key={index} className="carousel-slide">
+              <div className="image-wrapper">
+                <Image
+                  className="carousel_img"
+                  src={board.images.imageUrl}
+                  alt={`Board ${board.boardId}`}
+                  layout="fill"
+                  onClick={() => sendDetail}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <button className="carousel-prev" onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className="carousel-next" onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
       </div>
     </div>
   );
