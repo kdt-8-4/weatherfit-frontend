f58069a0f181a5b2d8a4000c5c6385524fe5d1c8
hyeiis <hyeis0807@gmail.com>
2023-12-06 22:31:37 +0900

    [Fix] #118, #56 css 및 기능 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,31 +6,73 @@ import "@/style/main.scss";
 import Image from "next/image";
 import BestItem from "@/component/BestItem";
 import BestCoordi from "@/component/BestCoordi";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { WeatherState } from "@/recoilAtom/WeatherState";
 
 export default function Mainpage() {
+  const weather = useRecoilValue(WeatherState);
+  const [isLoading, setIsLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    async function getTop5() {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: `https://www.jerneithe.site/category/tops?temp_min=${weather.min}&temp_max=${weather.max}`,
+        });
+
+        const response2 = await axios({
+          method: "GET",
+          // url: `https://www.jerneithe.site/board/tops?temp_min=${weather.min}&temp_max=${weather.max}`,
+          url: `https://www.jerneithe.site/board/tops?temp_min=0&temp_max=10`,
+        });
+
+        setCategories(response.data.result);
+        setIsLoading(false); // 로딩 완료 후 상태 업데이트
+        console.log("카테고리 top5", response.data.result);
+
+        setBoards(response2.data.content);
+        console.log("게시물 top5", response2.data.content);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getTop5();
+    setIsLoading(false);
+  }, [weather.max, weather.min]);
+
   return (
     <div className="container">
-      <header>
-        <div className="top">
-          <div className="img_wrap">
-            <Image
-              className="logo"
-              src="/images/logo2.svg"
-              alt="옷늘날씨"
-              width={150}
-              height={90}
-            />
-          </div>
-        </div>
-      </header>
-      <section className="main">
-        <MainWeather />
-        <br />
-        <BestItem />
-        <br />
-        <BestCoordi />
-      </section>
-      <Menubar />
+      {isLoading ? (
+        <div style={{ textAlign: "center" }}>Loading...</div>
+      ) : (
+        <>
+          <header>
+            <div className="top">
+              <div className="img_wrap">
+                <Image
+                  className="logo"
+                  src="/images/logo2.svg"
+                  alt="옷늘날씨"
+                  width={150}
+                  height={90}
+                />
+              </div>
+            </div>
+          </header>
+          <section className="main">
+            <MainWeather />
+            <br />
+            <BestItem categories={categories} />
+            <br />
+            <BestCoordi boards={boards} />
+          </section>
+          <Menubar />
+        </>
+      )}
     </div>
   );
 }
--- src/component/BestCoordi.tsx
+++ src/component/BestCoordi.tsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useRecoilValue } from "recoil";
-import { WeatherState } from "@/recoilAtom/WeatherState";
 import "@/style/best_main.scss";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
@@ -16,9 +14,8 @@ interface Board {
   };
 }
 
-export default function BestCoordi() {
-  const [boards, setBoards] = useState<Board[]>([]);
-  const weather = useRecoilValue(WeatherState);
+export default function BestCoordi({ boards }: { boards: Board[] }) {
+  // const [boards, setBoards] = useState<Board[]>([]);
   const router = useRouter();
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -39,26 +36,6 @@ export default function BestCoordi() {
     localStorage.setItem("getBoardId_local", JSON.stringify(board_id));
     router.push("/detail");
   };
-
-  //현재 최저, 최고온도에 따른 좋아요가 가장 많은 게시물 5개 가져오기
-  useEffect(() => {
-    async function getTop5() {
-      try {
-        const response = await axios({
-          method: "GET",
-          // url: `https://www.jerneithe.site/board/tops?temp_min=${weather.min}&temp_max=${weather.max}`,
-          url: `https://www.jerneithe.site/board/tops?temp_min=0&temp_max=10`,
-        });
-
-        setBoards(response.data.content);
-        console.log("게시물 top5", response.data.content);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    getTop5();
-  }, [weather.max, weather.min]);
 
   return (
     <div className="boardTop5-container">
